4b798820141a6791f85164ffac45d215db663b0c
takayuki-s <taka12yuki04@gmail.com>
2021-03-10 21:21:49 +0900

    アラート表示

--- app/javascript/components/Port.js
+++ app/javascript/components/Port.js
@@ -1,14 +1,18 @@
 import React from "react"
 import PropTypes from "prop-types"
+function showHello() {
+  alert('Hello')
+}
 
 class Port extends React.Component {
   render () {
     return (
-      <div className="box">
+      <div className="box" onClick={showHello}>
         Greeting: {this.props.greeting}
         <p>テストです！</p>
         <p>divで囲むと２つ以上の要素を表示できます！</p>
         <hr/>
+        
       </div>
     );
   }
