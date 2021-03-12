2c1fee7b72b17eef38b80f380c795fcb9608ab05
takayuki-s <taka12yuki04@gmail.com>
2021-03-12 16:25:20 +0900

    ２つ目のコンポーネント作成テスト

---
+++ app/javascript/components/Blog.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Blog extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        コンポーネント作成テスト
+      </React.Fragment>
+    );
+  }
+}
+
+export default Blog
