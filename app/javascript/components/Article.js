d571abc7d795392718e5bba9eda9e831295ed172
takayuki-s <taka12yuki04@gmail.com>
2021-03-13 22:01:37 +0900

    真偽値受け渡しテスト

--- app/javascript/components/Article.js
+++ app/javascript/components/Article.js
@@ -2,12 +2,21 @@ import React from "react"
 import PropTypes from "prop-types"
 
 const Article = (props) => {
+  let publishState = ""
+  if(props.isPublished){
+    publishState = "公開"
+
+  } else {
+    publishState = "非公開"
+  }
+
   return (
     <div>
       <h2>リアクトコンポーネントのprops受け渡しテスト</h2>
+      {props.title}
       <p>数字は{props.num}です</p>
       <p>著者は{props.author}です</p>
-      {props.title}
+      <p>{publishState}</p>
     </div>
   );
 }
