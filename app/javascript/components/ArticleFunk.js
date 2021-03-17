de50cf457ec75220f95186cf8d8cd2b34edbeb95
takayuki-s <taka12yuki04@gmail.com>
2021-03-17 11:22:01 +0900

    関数コンポーネントでstateを扱う

---
+++ app/javascript/components/ArticleFunk.js
@@ -0,0 +1,31 @@
+// import React from "react";
+import React, {useState} from 'react';
+
+import PropTypes from "prop-types"
+import LikeButton from "./LikeButton"
+
+const ArticleFunk = (props) => {
+  const [isPublished, togglePublished] = useState(false);
+
+  let publishState = ""
+  if(isPublished){
+    publishState = "公開"
+
+  } else {
+    publishState = "非公開"
+  }
+
+  return (
+    <div>
+      <h2>{props.title}</h2>
+      <p>{publishState}</p>
+      <label htmlFor="check">公開状況：</label>
+      <input type="checkbox" defaultChecked={isPublished} id="check" onClick={() => togglePublished(!isPublished)}/>
+      <br/><br/>
+      <LikeButton count={props.count}/>
+    </div>
+  );
+}
+
+
+export default ArticleFunk
---
+++ app/javascript/components/BlogFunk.js
@@ -0,0 +1,13 @@
+import React  from "react"
+import PropTypes from "prop-types"
+import ArticleFunk from "./ArticleFunk"
+import { render } from "react-dom"
+
+const BlogFunk = () => {
+  return<ArticleFunk 
+    title={"関数コンポーネントでstateを扱ってみよう"}
+  />
+}
+
+
+export default BlogFunk
