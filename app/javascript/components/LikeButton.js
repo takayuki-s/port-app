6ad2649f0ccff2baa59e51425cbc272d840d4d65
takayuki-s <taka12yuki04@gmail.com>
2021-03-18 09:39:59 +0900

    Revert "Revert "関数コンポーネントでライフサイクル（Deprecationあり修正予定）""
    This reverts commit 8055e0eb56522cca92a37fd11c843ed025dc857c.

--- app/javascript/components/LikeButton.js
+++ app/javascript/components/LikeButton.js
@@ -1,11 +1,27 @@
-import React from "react"
+import React, {useState, useEffect} from 'react';
 import PropTypes from "prop-types"
 
-const LikeButton = (props) => {
+const LikeButton = () => {
+  const [count, counter] = useState(0);
+
+  const countUp = () => {
+    counter(count + 1)
+  }
+
+  useEffect ( () => {
+    document.getElementById("counter").addEventListener("click", countUp)
+    if (count >= 10) {
+      counter(0)
+    }
+    return () => {
+      document.getElementById("counter").removeEventListener("click", countUp)
+    }
+  });
+
   return (
-    <button id={"counter"}>いいねの数：{props.count}</button>
+    <button id={"counter"}>いいねの数：{count}</button>
   )
-}
+};
 
 
 export default LikeButton
